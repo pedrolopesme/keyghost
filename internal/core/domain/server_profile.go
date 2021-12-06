476bb9abb6b42b8117d2fa94b56fa6dd12e8fe95
pedro.mendes <pedro.mendes@g.globo>
2021-12-06 14:25:38 -0300

    minor: add wellknown to OpenIDGhost Service

--- internal/core/domain/server_profile.go
+++ internal/core/domain/server_profile.go
@@ -4,5 +4,6 @@ import "github.com/pedrolopesme/keyghost/internal/core/domain/openid"
 
 type ServerProfile struct {
 	ProfileName string           `json:"profile_name"`
+	Realm       string           `json:"realm"`
 	Wellknown   openid.Wellknown `json:"wellknown"`
 }
--- internal/core/services/openid.go
+++ internal/core/services/openid.go
@@ -2,18 +2,33 @@ package services
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/pedrolopesme/keyghost/internal/core/domain"
+	"go.uber.org/zap"
 )
 
 type OpenIdGhost struct {
-	ctx     context.Context
+	logger  zap.Logger
 	profile domain.ServerProfile
 }
 
 func NewOpenIdGhost(ctx context.Context, profile domain.ServerProfile) *OpenIdGhost {
 	return &OpenIdGhost{
-		ctx:     ctx,
+		logger:  ctx.Value("logger").(zap.Logger),
 		profile: profile,
 	}
+}
+
+func (o OpenIdGhost) Wellknown() string {
+	wellknown, err := json.Marshal(o.profile.Wellknown)
+	if err != nil {
+		o.logger.Error(err.Error())
+		return ""
+	}
+
+	response := fmt.Sprintf(string(wellknown), o.profile.Realm)
+
+	return response
 }
